40d15e33d0afe73983ce73e4b077d6762e1a0262
Natalia Baldivia Gomes <natbg.9@gmail.com>
2020-10-15 20:06:49 -0300

    my first API

---
+++ acaicomdados.api/acaicomdados.api/Controllers/pessoaController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acaicomdados.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class pessoaController : ControllerBase
+    {
+        List<pessoa> tabelaFake;
+
+        public pessoaController()
+        {
+            tabelaFake = new List<pessoa>();
+
+            tabelaFake.Add(new pessoa() { DataNascimento = new DateTime(1991, 08, 09), estadocivil = estadocivil.Casado, Id = 1, Nome = "Natalia" });
+            tabelaFake.Add(new pessoa() { DataNascimento = new DateTime(1992, 02, 05), estadocivil = estadocivil.Casado, Id = 2, Nome = "Everton"});
+            tabelaFake.Add(new pessoa() { DataNascimento = new DateTime(1999, 01, 26), estadocivil = estadocivil.Solteiro, Id = 3, Nome = "Luana"});
+        }
+
+        // GET: api/pessoa
+        [HttpGet]
+        public IEnumerable<pessoa> Get()
+        {
+            return tabelaFake;
+        }
+
+        [HttpGet("{id}")]
+        public pessoa Get(int id)
+        {
+            return tabelaFake.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST: api/pessoa
+        [HttpPost]
+        public IEnumerable<pessoa> Post([FromBody] string value)
+        {
+            tabelaFake.Add(new pessoa { DataNascimento = new DateTime(1986, 06, 30), Id = 4, estadocivil = estadocivil.Casado, Nome = "Bololo" });
+            return tabelaFake;
+        }
+
+        // PUT: api/pessoa/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           // tabelaFake.Remove()
+        }
+    }
+}
---
+++ acaicomdados.api/acaicomdados.api/pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acaicomdados.api
+{
+    public class pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public estadocivil estadocivil { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    }
+}
